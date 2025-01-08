511cd55d21a35f6a9ef536b1ef08c3faacd642c7
yasin303 <myasinabdulhafiidh@gmail.com>
2025-01-08 11:45:47 +0700

    add dependencies cucumber

--- SeleniumProject/build.gradle.kts
+++ SeleniumProject/build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     implementation("org.seleniumhq.selenium:selenium-java:4.27.0")
     implementation("io.github.bonigarcia:webdrivermanager:5.9.2")
+    implementation("io.cucumber:cucumber-java:7.20.1")
+    testImplementation("io.cucumber:cucumber-junit:7.20.1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
