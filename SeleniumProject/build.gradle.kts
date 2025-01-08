2524768021fd39c0abf342831cc95c18a939301a
yasin303 <myasinabdulhafiidh@gmail.com>
2025-01-09 00:31:07 +0700

    update build gradle

--- SeleniumProject/build.gradle.kts
+++ SeleniumProject/build.gradle.kts
@@ -10,14 +10,15 @@ repositories {
 }
 
 dependencies {
-    implementation("org.seleniumhq.selenium:selenium-java:4.27.0")
-    implementation("io.github.bonigarcia:webdrivermanager:5.9.2")
-    implementation("io.cucumber:cucumber-java:7.20.1")
-    testImplementation("io.cucumber:cucumber-junit:7.20.1")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.seleniumhq.selenium:selenium-java:4.7.2")
+    testImplementation("io.github.bonigarcia:webdrivermanager:5.9.2")
+    testImplementation("io.cucumber:cucumber-java:7.15.0")
+    testImplementation("io.cucumber:cucumber-junit:7.15.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.4")
 }
 
 tasks.test {
-    useJUnitPlatform()
+    useJUnit()
 }
