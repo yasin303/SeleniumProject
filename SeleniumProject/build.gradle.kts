8f1ac3138a253bb8b9528b52f6615384b64ce4a5
yasin303 <myasinabdulhafiidh@gmail.com>
2025-01-10 18:18:06 +0700

    update cucumber test

--- SeleniumProject/build.gradle.kts
+++ SeleniumProject/build.gradle.kts
@@ -19,6 +19,35 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.4")
 }
 
-//tasks.test {
-//    useJUnit()
-//}
+tasks.test {
+    useJUnit()
+}
+
+configurations {
+    create("cucumberRuntime") {
+        extendsFrom(configurations["testImplementation"])
+    }
+}
+
+tasks.register("cucumber") {
+    description = "Running Cucumber Test"
+    dependsOn("assemble", "compileTestJava")
+
+    doLast {
+        javaexec {
+            mainClass.set("io.cucumber.core.cli.Main")
+            classpath = files(
+                configurations["cucumberRuntime"],
+                sourceSets["main"].output,
+                sourceSets["test"].output
+            )
+            args = listOf(
+                "--plugin", "html:reports/test-report.html",
+                "--plugin", "pretty",
+                "--glue", "com.yasin",
+                "--tags", System.getProperty("tags") ?: "",
+                "src/test/resources"
+            )
+        }
+    }
+}
